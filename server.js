53d79cb52ed102fcf315dbb4f318a0ea14fd3c8a
dennisloliver <doliver9110@gmail.com>
2022-01-22 01:48:06 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 const pokemon = require('./models/pokemon.js')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({
   extended: false
